594fd4c374c0c4f1a73dffdaf7f8c2349b462dfa
XuanHong2000 <xuanthuannguyen2000@gmail.com>
2022-11-11 07:18:59 +0700

    update

--- esp/data.h
+++ esp/data.h
@@ -18,7 +18,7 @@
 #define L3  D7
 #define L4  D8
 
-String ssid[] = { "Chinh Nghiep", "" }, password[] = { "03897.@!", "" };
+String ssid[] = { "Chinh Nghiep", "wififree" , "iphone" }, password[] = { "03897.@!", "hongbn12" , "hongbn123$" };
 uint8_t temp = 0, hum = 0;
 uint16_t gas = 0;
 uint64_t _time = 0;
