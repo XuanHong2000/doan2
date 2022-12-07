5f26f9888a6a54d585bf8171032ae0076ab056b1
XuanHong2000 <xuanthuannguyen2000@gmail.com>
2022-12-07 21:07:37 +0700

    update

--- python/main.py
+++ python/main.py
@@ -1,13 +1,22 @@
-import streamlit as st
-import time, random
+import time
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication("https://do-an-2-ute-default-rtdb.firebaseio.com/", None)
-data = firebase.get('BED_ROOM/data', None).split('-')
 path = ["BED_ROOM/nhiet_do", "BED_ROOM/do_am", "BED_ROOM/khi_ga", "BED_ROOM/cb_as", "BED_ROOM/ti_vi", "BED_ROOM/may_lanh", "BED_ROOM/den"]
 
-for i in range(len(path)):
-	firebase.put("/", path[i], int(data[i + 1]))
+while (true):
+        data = firebase.get('BED_ROOM/data', None).split('-')
+        
+        if (data[4] != str(firebase.get(path[3], None))):
+                firebase.put("/", path[3], int(data[4]))
+        if (data[5] != str(firebase.get(path[4], None))):
+                firebase.put("/", path[3], int(data[5]))
+        if (data[6] != str(firebase.get(path[5], None))):
+                firebase.put("/", path[3], int(data[6]))
+        if (data[7] != str(firebase.get(path[6], None))):
+                firebase.put("/", path[3], int(data[7]))
 
-time.sleep(2)
-st.experimental_rerun()
+        for i in range(3):
+                firebase.put("/", path[i], int(data[i + 1]))
+
+        time.sleep(2)
